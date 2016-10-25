6a1666a5e92eaf90f73a7b2dc65c8597488a496a
wenyanqi <yanqi.wyq@alibaba-inc.com>
2016-10-25 12:22:05 +0800

    change nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,6 +7,7 @@ import { Menu, Icon} from 'antd'
 const SubMenu = Menu.SubMenu;
 
 
+
 export default class Navigation extends Component{
 
   constructor(props) {
@@ -15,6 +16,8 @@ export default class Navigation extends Component{
 
     this.handleClick = this.handleClick.bind(this);
     this.onOpenChange = this.onOpenChange.bind(this);
+
+    this.mapPath = {};
   }
 
   initState() {
@@ -26,27 +29,36 @@ export default class Navigation extends Component{
 
   initKeyPath(menu) {
     //设计算法实现getKeyPath的功能，不需要再配置
-    
+
   }
   //
-  // generateNav(menu) {
-  //   let result = [];
-  //   let that = this;
-  //   _.each(menu, function(item, i){
-  //     if(item.children != null) {
-  //       result.push(
-  //         <SubMenu key={item.key} title={item.text} >
-  //             {that.generateNav(item.children)}
-  //         </SubMenu>
-  //       );
-  //     } else {
-  //       result.push (
-  //         <Menu.Item key={item.key} >{item.text}</Menu.Item>
-  //       );
-  //     }
-  //   });
-  //   return result;
-  // }
+  generateNav(menu, tempPath) {
+
+    let that = this,
+        result = [];
+
+    _.each(menu, function(item, i){
+
+      tempPath.push(item.key);
+      that.mapPath[item.key] = tempPath.slice();
+      // _.assign(that.mapPath, { key: tempPath});
+      if(item.children != null) {
+        result.push(
+          <SubMenu key={item.key} title={item.text} >
+              {that.generateNav(item.children, tempPath)}
+          </SubMenu>
+        );
+      } else {
+        result.push (
+          <Menu.Item key={item.key} >{item.text}</Menu.Item>
+        );
+      }
+
+      tempPath.pop();
+
+    });
+    return result;
+  }
 
   handleClick(e) {
     this.setState({current: e.key, openKeys: []});
@@ -54,6 +66,8 @@ export default class Navigation extends Component{
 
   onOpenChange(openKeys) {
    const latestOpenKey = openKeys.find(key => !(this.state.openKeys.indexOf(key) > -1));
+
+  //  console.log(this.getKeyPath(latestOpenKey));
    this.setState({ openKeys: this.getKeyPath(latestOpenKey) });
   }
 
@@ -65,10 +79,11 @@ export default class Navigation extends Component{
        sub3: ['sub2', 'sub3'],
        sub4: ['sub4'],
      };
-     return map[key] || [];
+     return this.map[key] || [];
    }
 
   render() {
+
 
     return (
       <Menu
@@ -100,6 +115,15 @@ export default class Navigation extends Component{
           <Menu.Item key="12"><Link to="">Option 12</Link></Menu.Item>
         </SubMenu>
       </Menu>
+      // <Menu
+      //    mode="inline"
+      //    openKeys={this.state.openKeys}
+      //    selectedKeys={[this.state.current]}
+      //    style={{ width: 240 }}
+      //    onOpenChange={this.onOpenChange}
+      //    onClick={this.handleClick}>
+      //   {this.generateNav(json, new Array())}
+      // </Menu>
     )
   }
 }
